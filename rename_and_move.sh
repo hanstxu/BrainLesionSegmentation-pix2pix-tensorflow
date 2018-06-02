49ffac88b528f1640460693a915074b2849595bf
Brandon Liu <bliu7654@gmail.com>
2018-06-01 20:40:33 -0700

    basic shell script. moves <number>/<type>/*.png to <type>/*-label.png

---
+++ rename_and_move.sh
@@ -0,0 +1,31 @@
+# For features
+# for dir in *
+# 	do
+# 		for folder in $dir/*
+# 			do
+# 				for file in $dir/Features/*.png
+# 					do
+
+# 						newFileName = ${file%%.*}-$dir.dcm.png
+# 						baseName=$(basename $newFileName)
+# 						echo mv "$file" "Features/$baseName"
+# 				done
+# 		done
+# done
+
+
+# For labels
+# for dir in *
+# 	do
+# 		for folder in $dir/*
+# 			do
+# 				for file in $dir/Labels/*.png
+# 					do
+
+# 						newFileName = ${file%%.*}-$dir.dcm.png
+# 						baseName=$(basename $newFileName)
+# 						echo mv "$file" "Labels/$baseName"
+# 				done
+# 		done
+# done
+
